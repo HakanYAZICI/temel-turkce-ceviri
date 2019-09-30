0988f8657564f283b8895a3ae1c6dad0b552ab4e
Hakan Yazıcı <hakan.yazici1@ogr.atauni.edu.tr>
2019-09-30 16:31:20 +0300

    Create tr-general.php
    laravel file

---
+++ tr-general.php
@@ -0,0 +1,217 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: HakanYAZICI https://github.com/HakanYAZICI/
+ * Date: 14.06.2019
+ * Time: 22:40
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    //roller
+
+    'super_admin' => 'Süper Yönetici',
+    'admin' => 'Yönetici',
+    'moderator' => 'Moderatör',
+    'editor' => 'Editör',
+    'reviewer' => 'Eleştirmen',
+    'member' => 'Üye',
+    'premium_member' => 'Ayrıcalıklı Üye',
+    'banned_member' => 'Yasaklı Üye',
+    'subscriber' => 'Abone',
+
+
+    //temel
+
+    'error' => 'Hata',
+    'success' => 'Başarılı',
+    'attention' => 'Dikkat',
+    'danger' => 'Tehlike',
+    'info' => 'Bilgi',
+    'tip' => 'İpucu',
+    'deleted' => 'Silinmiş',
+    'perfect' => 'Harika',
+    'incomplete' => 'Eksik',
+    'required' => 'Gerekli',
+    'optional' => 'İsteğe Bağlı',
+
+
+    //yayınlama
+
+    'draft' => 'Taslak',
+    'published' => 'Yayınlanmış',
+    'scheduled' => 'Zamanlanmış',
+    'publish' => 'Yayınla',
+
+
+    //Talimatlar
+    'log_in' => 'Giriş Yap',
+    'log_out' => 'Çıkış Yap',
+    'sign_up' => 'Kayıt Ol',
+    'accept' => 'Kabul Et',
+    'reject' => 'Kabul Etme',
+    'open' => 'Aç',
+    'close' => 'Kapat',
+    'edit' => 'Düzenle',
+    'delete' => 'Sil',
+    'hide' => 'Gizle',
+    'show' => 'Göster',
+    'undo' => 'Geri Al',
+    'redo' => 'Tekrarla',
+    'show_more' => 'Daha Fazla Gör',
+    'show_less' => 'Daha Az Gör',
+    'buy' => 'Satın Al',
+    'sell' => 'Sat',
+    'subscribe' => 'Abone Ol',
+    'unsubscribe' => 'Abonelikten Çık',
+    'ban' => 'Yasakla',
+    'unban' => 'Yasaklamayı Kaldır',
+
+
+    //uyarılar
+
+    'cannot_be_undone' => 'Bu İşlem Geri Alınamaz',
+    'not_authorized' => 'Bunun İçin Yetkiniz Yok',
+    'are_you_sure' => 'Emin misiniz?',
+    'logging_out' => 'Çıkış Yapılıyor',
+    'want_to_continue' => 'Devam Etmek İster Misiniz?',
+    'must_confirm_to_continue' => 'Devam Etmek İçin Onaylamanız Gerekli',
+
+
+    //bilgiler
+
+    'name' => 'İsim',
+    'username' => 'Kullanıcı Adı',
+    'email' => 'Elektronik Posta',
+    'phone' => 'Telefon',
+    'mobile_phone' => 'Cep Telefonu',
+    'address' => 'Adres',
+    'city' => 'Şehir',
+    'country' => 'Ülke',
+    'gender' => 'Cinsiyet',
+    'woman' => 'Kadın',
+    'man' => 'Erkek',
+    'other-gender' => 'Diğer',
+
+
+
+    //sayılar
+
+    'one' => 'On',
+    'two' => 'İki',
+    'three' => 'Üç',
+    'four' => 'Dört',
+    'five' => 'Beş',
+    'six' => 'Altı',
+    'seven' => 'Yedi',
+    'eight' => 'Sekiz',
+    'nine' => 'Dokuz',
+    'ten' => 'On',
+    'eleven' => 'On Bir',
+    'twelve' => 'On İki',
+    'thirteen' => 'On Üç',
+    'fourteen' => 'On Dört',
+    'fifteen' => 'On Beş',
+    'sixteen' => 'On Altı',
+    'seventeen' => 'On Yedi',
+    'eighteen' => 'On Sekiz',
+    'nineteen' => 'On Dokuz',
+    'twenty' => 'Yirmi',
+    'thirty' => 'Otuz',
+    'forty' => 'Kırk',
+    'fifty' => 'Elli',
+    'sixty' => 'Altmış',
+    'seventy' => 'Yetmiş',
+    'eighty' => 'Seksen',
+    'ninety' => 'Doksan',
+    'hundred' => 'Yüz',
+    'thousand' => 'Bin',
+    'million' => 'Milyon',
+    'billion' => 'Milyar',
+    'trillion' => 'Trilyon',
+    'quadrillion' => 'Katrilyon',
+
+
+    //mevsimler
+
+    'spring' => 'İlkbahar',
+    'summer' => 'Yaz',
+    'autumn' => 'Sonbahar',
+    'fall' => 'Güz',
+    'winter' => 'Kış',
+
+
+    //zaman birimleri
+
+    'second' => 'Saniye',
+    'minute' => 'Dakika',
+    'hour' => 'Saat',
+    'day' => 'Gün',
+    'week' => 'Hafta',
+    'month' => 'Ay',
+    'year' => 'Yıl',
+    'annual' => 'Yıllık',
+
+
+    //aylar
+
+    'january' => 'Ocak',
+    'february' => 'Şubat',
+    'march' => 'Mart',
+    'april' => 'Nisan',
+    'may' => 'Mayıs',
+    'june' => 'Haziran',
+    'july' => 'Temmuz',
+    'august' => 'Ağustos',
+    'september' => 'Eylül',
+    'october' => 'Ekim',
+    'november' => 'Kasım',
+    'december' => 'Aralık',
+
+    //Günler
+
+    'monday' => 'Pazartesi',
+    'tuesday' => 'Salı',
+    'wednesday' => 'Çarşamba',
+    'thursday' => 'Perşembe',
+    'friday' => 'Cuma',
+    'saturday' => 'Cumartesi',
+    'sunday' => 'Pazar',
+
+    //Dün, Bugün, Yarın vs.
+
+    'yesterday' => 'Dün',
+    'today' => 'Bugün',
+    'tomorrow' => 'Yarın',
+    'before' => 'Önce',
+    'after' => 'Sonra',
+    'now' => 'Şimdi',
+    'past' => 'Geçmiş',
+    'future' => 'Gelecek',
+
+    //renkler
+
+    'red' => 'Kırmızı',
+    'blue' => 'Mavi',
+    'yellow' => 'Sarı',
+    'green' => 'Yeşil',
+    'black' => 'Siyah',
+    'white' => 'Beyaz',
+    'purple' => 'Mor',
+    'pink' => 'Pembe',
+
+
+];
